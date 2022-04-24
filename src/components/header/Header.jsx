c1bfc037f8f894f3fe95959a2cc8bbc6067ac9d4
uzma000 <uzmak3296@gmail.com>
2022-04-24 16:21:10 +0100

    made header component
    made header component, done CSS for header and made it responsive.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/Uzma.jpg";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container" id="home">
+        <h5>Hello I am</h5>
+        <h1>Uzma Khan</h1>
+        <h5 className="text-light">Fullstack Web Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
