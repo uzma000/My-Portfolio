de7d4ada556cf724b39c927f382475d69b5846db
uzma000 <uzmak3296@gmail.com>
2022-04-25 17:38:57 +0100

    made navBar
    made nav bar, done some CSS for it and made few changes

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./contacts.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return <section id="contact">Contact</section>;
 };
 
 export default Contact;
